0e20994857811fe37506b1843faaed0ed2502286
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-08-16 16:00:12 +0200

    new settings class

---
+++ src/models/settings_new.js
@@ -0,0 +1,38 @@
+
+class Settings{
+    constructor(
+        storeID,
+        parameter,
+        value
+    ){
+        this.storeID = storeID !== null && storeID !== undefined ? storeID : this.throwError("storeID");
+        this.parameter = parameter !== null && parameter !== undefined ? parameter : this.throwError("parameter");
+        this.value = value !== null && value !== undefined ? value : this.throwError("value");
+    }
+
+    throwError(property) {
+        throw new Error(`Missing property ${property}`);
+    }
+
+    updateParameter(){
+        // return svc.storeService.setSettings(this);
+    }
+
+    // function for changing logo and background image.
+
+    get storeID(){
+        return this._storeID;
+    }
+
+    set storeID(storeID){
+        this._storeID = storeID;
+    }
+
+    get parameter(){
+        return this._parameter;
+    }
+
+    set parameter(parameter){
+        this._parameter = parameter;
+    }
+}
