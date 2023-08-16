6506fdfb6256d96efae840354b08a9b48dfa585f
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-08-16 16:01:32 +0200

    imported all sql queries

--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -4,46 +4,182 @@ class OrderRepository extends AbstractRepository{
     super(database);
   }
 
-  placeOrder(EMPLOYEE_NAME, PACKAGE_SIZE, DELIVERYDATE, CUSTOM_DROPPOFF_PLACE, STORE_ID) {
-    let sql = `
-      INSERT INTO DELIVERY_ORDER (TIMESTAMP, EMPLOYEE_NAME, PACKAGE_SIZE, DELIVERYDATE, CUSTOM_DROPPOFF_PLACE, STORE_ID) 
-      VALUES (?, ?, ?, ?, ?, ?)`;
-    const currentDate = new Date();
-    let params = [currentDate, EMPLOYEE_NAME, PACKAGE_SIZE, DELIVERYDATE, CUSTOM_DROPPOFF_PLACE, STORE_ID];
-    return this.database.queryWithoutResponse(sql, params);
-  }
-
-  cancelOrder(ORDER_ID) {
+  cancelOrder(ORDER_ID){
     let sql = "DELETE FROM DELIVERY_ORDER WHERE ORDER_ID = ?";
     let params = [ORDER_ID];
     return this.database.queryWithoutResponse(sql, params);
-  }
+}
 
-  allOrders(STORE_ID) {
+  allOrders(STORE_ID){
     let sql = "SELECT * FROM DELIVERY_ORDER WHERE STORE_ID = ?";
-    let params = [STORE_ID];
+    let params = [STORE_ID]
     return this.database.queryAllData(sql, params);
-  }
+}
 
-  // Add the rest of your functions as methods of the class
+ allOrdersByEmployee(EMPLOYEE_NAME){
+    let sql = "SELECT * FROM DELIVERY_ORDER WHERE EMPLOYEE_NAME = ?";
+    let params = [EMPLOYEE_NAME];
+    return this.database.queryAllData(sql);
+}
 
-  insertOrder(timestamp, employeeName, packageSize, deliveryDate, customDropoffPlace, storeID, addressID) {
-    let sql = `
-      INSERT INTO DELIVERY_ORDER (timestamp, employeeName, packageSize, deliveryDate, customDropoffPlace, storeID, addressID) 
-      VALUES (?, ?, ?, ?, ?, ?, ?);`;
+insertOrder(
+    timestamp, 
+    employeeName, 
+    packageSize, 
+    deliveryDate, 
+    customDropoffPlace, 
+    storeID,
+    addressID){
+    let sql = 
+    `
+    INSERT INTO
+        DELIVERY_ORDER (timestamp, employeeName, packageSize, deliveryDate, customDropoffPlace, storeID, addressID)
+    VALUES
+        (?, ?, ?, ?, ?, ?, ?);
+    `;
     let params = [timestamp, employeeName, packageSize, deliveryDate, customDropoffPlace, storeID, addressID];
     return this.database.insertTuple(sql, params);
-  }
+}
 
-  // Add more methods...
+ insertHandlingInfo(orderID, handlingInfo){
+    let sql = 
+    `
+    INSERT INTO
+        HANDLINGINFO (orderID, handlingInfo)
+    VALUES
+        (?, ?);
+    `;
+    let params = [orderID, handlingInfo];
+    return this.database.insertTuple(sql, params);
+}
 
-  deleteOrder(orderID) {
-    let sql = "DELETE FROM DELIVERY_ORDER WHERE ORDER_ID = ?";
+ insertRecipient(orderID, firstName, lastName, addressID){
+    let sql = 
+    `
+    INSERT INTO
+        RECIPIENT (orderID, firstName, lastName, addressID)
+    VALUES
+        (?, ?, ?, ?);
+    `;
+    let params = [orderID, firstName, lastName, addressID];
+    return this.database.insertTuple(sql, params);
+}
+
+ selectOrderID(timestamp, employeeName, packageSize, deliveryDate, customDropoffPlace, storeID, addressID){
+    let sql = 
+    `
+    SELECT 
+        orderID
+    FROM 
+        DELIVERY_ORDER
+    WHERE 
+        timestamp = ? 
+    AND 
+        employeeName = ? 
+    AND 
+        packageSize = ? 
+    AND 
+        deliveryDate = ? 
+    AND 
+        customDropoffPlace = ? 
+    AND 
+        storeID = ? 
+    AND 
+        addressID = ?;
+    `;
+    let params = [timestamp, employeeName, packageSize, deliveryDate, customDropoffPlace, storeID, addressID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectAllOrdersOfStore(storeID){
+    let sql =
+    `
+    SELECT
+        o.orderID,
+        o.timestamp,
+        o.employeeName,
+        o.packageSize,
+        o.deliveryDate,
+        o.customDropoffPlace,
+        h.handlingInfo,
+        r.firstName,
+        r.lastName,
+        a.street,
+        a.houseNumber,
+        z.ZIP
+    FROM
+        DELIVERY_ORDER o
+    JOIN
+        HANDLINGINFO h ON o.orderID = h.orderID
+    JOIN
+        RECIPIENT r ON o.orderID = r.orderID
+    JOIN
+        ADDRESS a ON o.addressID = a.addressID
+    JOIN
+        ZIP z ON a.zipID = z.zipID
+    WHERE
+        storeID = ?;
+    ORDER BY
+        o.orderID ASC;
+    `;
+    let params = [storeID];
+    return this.database.queryAllData(sql, params);
+}
+
+ selectHandlingInfo(orderID){
+    let sql = 
+    `
+    SELECT
+        *
+    FROM 
+        HANDLINGINFO
+    WHERE
+        orderID = ?;
+    `;
+    let params = [orderID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectRecipient(orderID){
+    let sql = 
+    `
+    SELECT 
+        *
+    FROM
+        RECIPIENT
+    WHERE
+        orderID = ?;
+    `;
+    let params = [orderID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ deleteOrder(orderID){
+    let sql = "DELETE FROM ORDER WHERE ORDER_ID = ?";
     let params = [orderID];
     return this.database.queryWithoutResponse(sql, params);
-  }
+}
 
-  // Add more methods...
+ deleteHandlingInfo(orderID){
+    let sql = 
+    `
+    SELECT
+        *
+    FROM
+        HANDLING_INFO
+    WHERE
+        orderID = ?;
+    `;
+    let params = [orderID];
+    return this.database.queryWithoutResponse(sql, params);
+}
+
+ deleteRecipient(firstname, lastname){
+    let sql = "DELETE FROM RECIPIENT WHERE firstname = ? AND lastname";
+    let params = [firstname, lastname];
+    return this.database.queryWithoutResponse(sql, params);
+}
+
 }
 
 module.exports = OrderRepository;
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -4,39 +4,221 @@ class UserRepository extends AbstractRepository{
     super(database);
   }
 
-  getPasswordByName(username) {
+   getPasswordByName(username){
     let sql = "SELECT password FROM LOGINCREDENTIALS WHERE username = ?";
-    let params = [username];
+    let params = [username]
     return this.database.queryOneDataSet(sql, params);
-  }
+}
 
-  addRegisterCode(code) {
+ addRegisterCode(code){
     let sql = "INSERT INTO REGISTRATION_CODES (CODE) VALUES (?)";
     let params = [code];
     return this.database.updateTable(sql, params);
-  }
+}
 
-  getRegisterCode(code) {
+ getRegisterCode(code){
     let sql = "SELECT CODE FROM REGISTRATION_CODES WHERE CODE = ?";
     let params = [code];
     return this.database.queryOneDataSet(sql, params);
-  }
+}
 
-  getExistingStore(storeName){
+ getStoreDataByUsername(username){
+    let sql =
+    `
+    SELECT
+        storeID,
+        storeName,
+        owner,
+        logo,
+        telephone,
+        email
+    FROM
+        STORE
+    WHERE
+        username = ?;
+    `;
+    let params = [username];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ getExistingStore(storeName){
     let sql = "SELECT storeName FROM STORE WHERE storeName = ?";
     let params = [storeName];
     return this.database.queryOneDataSet(sql, params);
 }
 
-  // ... Other methods ...
+ storeDetails(){
+    let sql = 
+    `
+    SELECT
+        s.storeID AS id,
+        s.storeName AS name,
+        s.owner AS owner,
+        a.street AS street,
+        a.houseNumber AS houseNumber,
+        z.zip AS zip,
+        s.telephone AS telephone,
+        s.email AS email,
+        s.logo AS logo,
+        s.backgroundImage AS backgroundImage,
+        a.longitude AS longitude,
+        a.latitude AS latitude
+    FROM
+        STORE s
+    JOIN
+        ADDRESS a ON s.addressID = a.addressID
+    JOIN
+        ZIP z ON a.zipID = z.zipID
+    ORDER BY
+        s.storeID DESC;
+    `;
+    return this.database.queryAllData(sql);
+}
 
-  // Add the rest of your functions as methods of the class
+ insertLoginCredentials(username, password){
+    let sql =
+    `
+    INSERT INTO
+        LOGINCREDENTIALS (username, password)
+    VALUES
+        (?, ?);
+    `;
+    let params = [username, password];
+    return this.database.insertTuple(sql, params);
+}
 
-  updatePassword(username, password) {
+ insertStore(storeName, owner, logo, backgroundImage, telephone, email, zipID, addressID, username){
+    let sql = 
+    `
+    INSERT INTO
+        STORE (storeName, owner, logo, backgroundImage, telephone, email, zipID, addressID, username)
+    VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?);
+    `;
+    let params = [storeName, owner, logo, backgroundImage,telephone, email, zipID, addressID, username];
+    return this.database.queryWithoutResponse(sql, params);
+}
+
+ insertAddress(street, houseNumber, longitude, latitude, zipID){
+    let sql = 
+    `
+    INSERT INTO
+        ADDRESS (street, houseNumber, longitude, latitude, zipID)
+    VALUES
+        (?, ?, ?, ?, ?);
+    `;
+    let params = [street, houseNumber, longitude, latitude, zipID];
+    return this.database.insertTuple(sql, params);
+}
+
+ selectUsernameByStoreID(storeID){
+    let sql = "SELECT username FROM STORE WHERE storeID = ?";
+    let params = [storeID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectPasswordByUsername(username){
+    let sql = "SELECT password FROM LOGINCREDENTIALS WHERE username = ?";
+    let params = [username];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectStoreByStoreID(storeID){
+    let sql = "SELECT * FROM STORE WHERE storeID = ?";
+    let params = [storeID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectStoreID(storeName){
+    let sql = "SELECT storeID FROM STORE WHERE storeNAME = ?";
+    let params = [storeName];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectAddressByAddressID(addressID){
+    let sql = "SELECT * FROM ADDRESS WHERE addressID = ?";
+    let params = [addressID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectAddressIDByStoreID(storeID){
+    let sql = "SELECT addressID FROM STORE WHERE storeID = ?";
+    let params = [storeID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectAddressID(street, houseNumber, zipID){
+    let sql =
+    `
+    SELECT
+        ADDRESSID
+    FROM
+        ADDRESS
+    WHERE
+        street = ?
+    AND
+        houseNumber = ?
+    AND
+        zipID = ?;
+    `;
+    let params = [street, houseNumber, zipID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ selectZIPByZipID(zipID){
+    let sql = "SELECT ZIP FROM ZIP WHERE zipID = ?";
+    let params = [zipID];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ async selectZipID(zip) {
+    let sql = "SELECT ZIPID FROM ZIP WHERE ZIP = ?";
+    let params = [zip];
+    return this.database.queryOneDataSet(sql, params);
+}
+
+ updateStore(storeID, storeName, owner, telephone, email, zipID, addressID){
+    let sql = 
+    `
+    UPDATE
+        STORE
+    SET
+        storeName = ?,
+        owner = ?,
+        telephone = ?,
+        email = ?,
+        zipID = ?,
+        addressID = ?
+    WHERE
+        storeID = ?;
+    `;
+    let params = [storeName, owner, telephone, email, zipID, addressID, storeID];
+    return this.database.updateTable(sql, params);
+}
+
+ updateAddress(addressID, street, houseNumber, longitude, latitude, zipID){
+    let sql =
+    `
+    UPDATE
+        ADDRESS
+    SET
+        street = ?,
+        houseNumber = ?,
+        longitude = ?,
+        latitude = ?,
+        zipID = ?
+    WHERE
+        addressID = ?;
+    `;
+    let params = [street, houseNumber, longitude, latitude, zipID, addressID];
+    return this.database.updateTable(sql, params);
+}
+
+ updatePassword(username, password){
     let sql = "UPDATE LOGINCREDENTIALS SET password = ? WHERE username = ?";
     let params = [password, username];
     return this.database.updateTable(sql, params);
-  }
+}
 }
 
 module.exports = UserRepository;
